dfb0a92ed493db8eb6c55299ca6feb2bb602f307
giyeon95 <giyeon15@gmail.com>
2022-05-29 00:21:15 +0900

    feat: 2. remove static

--- src/main/java/function/PrintPrimes.java
+++ src/main/java/function/PrintPrimes.java
@@ -1,13 +1,14 @@
 package function;
 
 public class PrintPrimes {
-    public static void main(String[] args) {
-        PrimePrintHelper.invoke();
+
+    public void main(String[] args) {
+        new PrimePrintHelper().invoke();
     }
 
-    private static class PrimePrintHelper {
+    public class PrimePrintHelper {
 
-        private static void invoke() {
+        private void invoke() {
             final int numberOfPrimes = 1000;
             final int linesPerPage = 50;
             final int columns = 4;
@@ -40,10 +41,12 @@ public class PrintPrimes {
                     n = 2;
                     possiblyPrime = true;
                     while (n < ord && possiblyPrime) {
-                        while (multiples[n] < candidate)
+                        while (multiples[n] < candidate) {
                             multiples[n] = multiples[n] + primes[n] + primes[n];
-                        if (multiples[n] == candidate)
+                        }
+                        if (multiples[n] == candidate) {
                             possiblyPrime = false;
+                        }
                         n = n + 1;
                     }
                 } while (!possiblyPrime);
@@ -55,12 +58,14 @@ public class PrintPrimes {
                 pageoffset = 1;
                 while (pageoffset <= numberOfPrimes) {
                     System.out.println("The First " + numberOfPrimes +
-                            " Prime Numbers --- Page " + pagenumber);
+                        " Prime Numbers --- Page " + pagenumber);
                     System.out.println("");
                     for (rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
-                        for (column = 0; column < columns; column++)
-                            if (rowoffset + column * linesPerPage <= numberOfPrimes)
+                        for (column = 0; column < columns; column++) {
+                            if (rowoffset + column * linesPerPage <= numberOfPrimes) {
                                 System.out.format("%10d", primes[rowoffset + column * linesPerPage]);
+                            }
+                        }
                         System.out.println("");
                     }
                     System.out.println("\f");
